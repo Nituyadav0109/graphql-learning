36de4f57712597734dc2bc3803935c6723311e8d
Nitu Yadav <126680562+Nituyadav0109@users.noreply.github.com>
2025-10-22 11:22:51 +0530

    Delete canyon-bookhub directory

--- canyon-bookhub/src/server.js
+++
@@ -1,8 +0,0 @@
-import { CanyonServer } from "@canyon/server";
-import { resolvers } from "./resolvers.js";
-import schema from "./schema.graphql";
-
-const server = new CanyonServer({ typeDefs: schema, resolvers });
-server.listen({ port: 4001 }).then(({ url }) => {
-  console.log(`🚀 Canyon BookHub running at ${url}`);
-});
